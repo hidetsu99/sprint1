1200c267cfd5777fae0819b0a6903ac12789adfb
PatoSuarez1 <pato.s_z@hotmail.com>
2017-09-29 17:07:22 -0300

    Add files via upload

---
+++ nuevo.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/nuevo.css">
+  <title>Nombre</title>
+</head>
+<body>
+  <div class="container">
+    <!--HEADER-->
+    <header>
+      <i class="fa fa-bars fa-2x" aria-hidden="true" data-toggle="modal" data-target="#faBars"></i>
+      <img src="img/logo.jpg" alt="logo" class="logo_header">
+      <i class="fa fa-search fa-2x" aria-hidden="true"></i>
+
+    </header>
+    <!--BANNER-->
+    <div class="banner">
+      <a href="formulario.php"><button type="submit" class="login">Registrarse</button></a>
+      <form class="form-group" action="nuevo.php" method="post" enctype="multipart/form-data">
+        <div>
+          <button type="submit" class="singup"> Iniciar Sesion</button>
+          <input type="email" name="email" class="email" placeholder="email">
+          <input type="password" name="password" class="password" placeholder="clave">
+
+        </div>
+      </form>
+      
+    </div>
+    <!--PRODUCTS-->
+    <div class="products_body">
+      <h2 class="body_title">Servicios actuales</h2>
+      <section class="section_body">
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Eventos</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Salud</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Educacion</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Deporte</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Educacion</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+          <article class="product">
+              <h3 class="article_title">
+                <span class="span_h3">
+                  <i class="fa fa-shower fa-2x" aria-hidden="true"></i>
+                </span>
+                <span class="product_name">Seguridad</span>
+              </h3>
+              <ul class="ul_product">
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+                <li class="li_product">
+                  <a href="#" class="a_products">Prosfesion o trabajo
+                    <span class="num_product">456</span>
+                  </a>
+                </li>
+              </ul>
+          </article>
+
+      </section>
+    </div>
+    <!--FOOTER-->
+    <footer class="main-footer">
+      <ul class="ul_footer">
+        <div class="help-footer">
+          <li class="url_footer"><a href="faq.php">FAQ</a></li>
+          <li class="url_footer"><a href="#">Terms y Conditions</a></li>
+          <li class="url_footer"><a href="#">Help</a></li>
+        </div>
+        <br class="br_footer">
+        <div class="icons-footer">
+            <li class="icons_footer"><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+            <li class="icons_footer"><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li class="icons_footer"><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i></a></li>
+        </div>
+      </ul>
+    </footer>
+  </div>
+</body>
+</html>
